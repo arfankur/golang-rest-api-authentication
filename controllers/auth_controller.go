a7e05f337d0949fd773b955145332e87f521b34b
arfankur <arfankurnianto29@gmail.com>
2025-04-23 14:43:59 +0700

    commit message

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -18,6 +18,7 @@ func Register(c *gin.Context) {
 	}
 
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash password"})
 		return
